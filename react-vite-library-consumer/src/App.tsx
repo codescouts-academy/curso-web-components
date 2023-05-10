e058c2950da2777a28b97489a183d5a6d6639a47
Damián Pumar <damianpumar@gmail.com>
2023-05-10 10:50:18 +0200

    ✨ Add example

---
+++ react-vite-library-consumer/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "react-vite-library";
+import "react-vite-library/style.css";
+
+function App() {
+  return (
+    <>
+      <h2>Consumer</h2>
+      <app-component></app-component>
+    </>
+  );
+}
+
+export default App;
---
+++ react-vite-library/src/index.ts
@@ -0,0 +1,22 @@
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import reactToWebComponent from "react-to-webcomponent";
+import { App } from "./App";
+
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      "app-component": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+
+customElements.define(
+  "app-component",
+  reactToWebComponent(App, React, ReactDOMClient)
+);
